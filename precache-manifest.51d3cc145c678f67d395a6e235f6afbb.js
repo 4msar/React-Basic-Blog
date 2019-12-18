3d74c73ead51d0842a90bb8aaa950ec27bd53505
Saiful Alam <msa4rakib@gmail.com>
2019-12-19 01:21:55 +0600

    Updates

---
+++ precache-manifest.51d3cc145c678f67d395a6e235f6afbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51863819084b068c54fbbb24c65bf240",
+    "url": "/React-Basic-Blog/index.html"
+  },
+  {
+    "revision": "fe169768f8eb3e91d763",
+    "url": "/React-Basic-Blog/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "080e433e9c5ded1b1997",
+    "url": "/React-Basic-Blog/static/css/main.5d911d19.chunk.css"
+  },
+  {
+    "revision": "fe169768f8eb3e91d763",
+    "url": "/React-Basic-Blog/static/js/2.d6d0c0bd.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/React-Basic-Blog/static/js/2.d6d0c0bd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "080e433e9c5ded1b1997",
+    "url": "/React-Basic-Blog/static/js/main.d7079cd6.chunk.js"
+  },
+  {
+    "revision": "c7735928f41a68ad8556",
+    "url": "/React-Basic-Blog/static/js/runtime-main.a7dcd4a6.js"
+  }
+]);
